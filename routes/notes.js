6c96b6f4c91dac308db89ca4de1f9fec6a5fa04d
Kevin LaCarrubba <Kevin.LaCarrubba@yahoo.com>
2021-09-02 16:14:11 -0400

    get note via uuid

--- routes/notes.js
+++ routes/notes.js
@@ -10,3 +10,15 @@ const {
 notes.get("/", (req, res) => {
   readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
+
+notes.get("/:note_id", (req, res) => {
+  const noteID = req.params.note_id;
+  readFromFile("./db/db.json")
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const output = json.filter((note) => note.note_id === noteID);
+      return output.length > 0
+        ? res.json(output)
+        : res.json("No tip with that ID");
+    });
+});
