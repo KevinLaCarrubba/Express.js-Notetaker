32197cb181b00b64fcb4807e8dd567b0197216cd
Kevin LaCarrubba <Kevin.LaCarrubba@yahoo.com>
2021-09-02 17:26:16 -0400

    need to fix pathing issue

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const path = require("path");
 const api = require("./routes/index.js");
-
+const path = require("path");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/api", api);
 
 app.get("/notes", (req, res) =>
