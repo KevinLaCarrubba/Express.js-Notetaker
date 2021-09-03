3a434158e7c30cd828cab2285517fad064b48872
Kevin LaCarrubba <Kevin.LaCarrubba@yahoo.com>
2021-09-03 15:30:30 -0400

    fixed pathing issue and id issue

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -1,25 +1,33 @@
+//bring in npm requirements
 const fs = require("fs");
 const util = require("util");
 
+//promisify fs.readfile
 const readFromFile = util.promisify(fs.readFile);
 
+//set writeToFile function (destination, content)
 const writeToFile = (destination, content) =>
+//JSON.stringify content, 3 lines
   fs.writeFile(destination, JSON.stringify(content, null, 3), (err) =>
     err
       ? console.error(err)
       : console.info(`\nData was written to ${destination}`)
   );
-
+//set readAndAppend function (conent, file location)
 const readAndAppend = (content, file) => {
+  //read files(name, type, err, data)
   fs.readFile(file, "utf8", (err, data) => {
     if (err) {
       console.error(err);
     } else {
+      //parse incoming data
       const parsedData = JSON.parse(data);
+      //push parsed data to append
       parsedData.push(content);
+      //write file (file, parsed data)
       writeToFile(file, parsedData);
     }
   });
 };
-
+//export readFromFile, readAndAppend, writeToFile
 module.exports = { readFromFile, readAndAppend, writeToFile };
--- routes/notes.js
+++ routes/notes.js
@@ -1,3 +1,4 @@
+//const var requirements
 const notes = require("express").Router();
 const { v4: uuidv4 } = require("uuid");
 
@@ -7,48 +8,64 @@ const {
   writeToFile,
 } = require("../helpers/fsUtils");
 
+//notes get root file 
 notes.get("/", (req, res) => {
+  //read from database. then take data and parse it
   readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
-notes.get("/:note_id", (req, res) => {
-  const noteID = req.params.note_id;
+//notes get note_uuid
+notes.get("/:id", (req, res) => {
+  //set noteID to param.note_id
+  const noteID = req.params.id;
+  //read from database
   readFromFile("./db/db.json")
+  //parse data
     .then((data) => JSON.parse(data))
     .then((json) => {
-      const output = json.filter((note) => note.note_id === noteID);
+      //filter through output of note to find note.note_id thats === noteID
+      const output = json.filter((note) => note.id === noteID);
+      //return if there is an output
       return output.length > 0
         ? res.json(output)
-        : res.json("No tip with that ID");
+        : res.json("No note with that ID");
     });
 });
-
-notes.delete("/:note_id", (req, res) => {
-  const noteID = req.params.note_id;
+//notes. to delete get uuid
+notes.delete("/:id", (req, res) => {
+  //set noteID to param.note_id
+  const noteID = req.params.id;
+  //read from database
   readFromFile("./db/db.json")
+  //parse the data
     .then((data) => JSON.parse(data))
     .then((json) => {
-      const output = json.filter((note) => note.note_id !== noteID);
+      //filter to find matching ID to delete
+      const output = json.filter((note) => note.id !== noteID);
+      console.log(output)
+      //rewrite to file after deleting
       writeToFile("./db/db.json", output);
       res.json(`Item ${noteID} has been deleted.`);
     });
 });
-
+//create new note to post
+//get notes.post root folder
 notes.post("/", (req, res) => {
+  //destructure database to req.body;
   const { title, text } = req.body;
-
+//if there is content 
   if (req.body) {
     const postItNote = {
       title,
       text,
-      note_id: uuidv4(),
+      id: uuidv4(),
     };
-
+//append postItNote object to file
     readAndAppend(postItNote, "./db/db.json");
     res.json("Note added.");
   } else {
     res.error("Note add failed.");
   }
 });
-
+//export notes
 module.exports = notes;
